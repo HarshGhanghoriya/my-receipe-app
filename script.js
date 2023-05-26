83d0e603442b9127ea3796559f28f1e20652ad85
Jitesh Kumar Shivgan <105727244+jiteshshivgan@users.noreply.github.com>
2023-05-27 00:38:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ mealsContainer.addEventListener("click", e=>{
       const mealID=addToFavourite.getAttribute("data-mealId");
       console.log(mealID);
       localStorage.setItem(i, mealID);
+       addToFavourite.innerHTML = "Added";
       i++;
    }
      
